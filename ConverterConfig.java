ad9ee57d3f4a71438cb47a63e06ca9013c100a78
Kirito-C <149937627+Kirito-C@users.noreply.github.com>
2025-06-16 10:14:19 +0200

    created first file

---
+++ ConverterConfig.java
@@ -0,0 +1,17 @@
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+public class ConverterConfig {
+    String folderPath = "config/";
+    String configFile = "config.json";
+    String configFilePath = folderPath + configFile;
+
+    try {
+
+    } catch () {
+
+    }
+
+    private Type mapType = new TypeToken<Map<String, String>>() {}.getType();
+    private
+}
