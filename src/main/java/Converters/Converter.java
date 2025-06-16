b20ea50d34d218fe7b8b443bb68f7a4537bb5ce5
GianlucaPletti07 <gianlucapleyt@gmail.com>
2025-06-16 16:01:17 +0200

    Update FileTypeConverter.java

---
+++ src/main/java/Converters/Converter.java
@@ -0,0 +1,7 @@
+package Converters;
+
+import java.io.File;
+
+public interface Converter {
+    public File convert(File srcFile);
+}
--- src/main/java/FileTypeConverter.java
+++ src/main/java/FileTypeConverter.java
@@ -7,6 +7,9 @@ public class FileTypeConverter {
     public static void main(String[] args) throws IOException {
         ConverterConfig config = new ConverterConfig();
         Thread watcherThread = new Thread(new DirectoryWatcher("C:\\Users\\DELLMuletto\\IdeaProjects\\convertitoreFile\\src\\input"));
+
+        //test
+
         watcherThread.setDaemon(true);
         watcherThread.start();
         while(true){}
