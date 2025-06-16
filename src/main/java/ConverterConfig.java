40df0eeaeccad898c47bf8c27927b5652c2d3019
Kirito-C <149937627+Kirito-C@users.noreply.github.com>
2025-06-16 11:35:41 +0200

    config.json created

--- src/main/java/ConverterConfig.java
+++ src/main/java/ConverterConfig.java
@@ -3,29 +3,33 @@ import com.google.gson.reflect.TypeToken;
 
 import java.io.FileReader;
 import java.lang.reflect.Type;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.Map;
 
 public class ConverterConfig {
-    private String folderPath = "config/";
-    private String configFile = "config.json";
+    private final String folderPath = "config/";
+    private final String configFile = "config.json";
     private String configFilePath = "";
     private Gson gson;
     private Type mapType;
 
     public ConverterConfig() {
-        this.folderPath = folderPath;
-        this.configFile = configFile;
         this.configFilePath = folderPath + configFile;
 
         try {
-            fileReader = new FileReader(configFilePath);
+            FileReader fileReader = new FileReader(configFilePath);
             gson = new Gson();
             mapType = new TypeToken<HashMap<String, LinkedList<String>>>() {}.getType();
 
-            Map<String, String> configMap = gson.fromJson(fileReader, mapType);
+            Map<String, LinkedList<String>> configMap = gson.fromJson(fileReader, mapType);
 
             for(String key : configMap.keySet()) {
-                System.out.println(key + ": " + for(String value : configMap.get(key)) {System.out.print(value + " - ");});
+                System.out.print(key + ": ");
+                for(String value : configMap.get(key)) {
+                    System.out.print(value + " - ");
+                }
+                System.out.println();
             }
 
             /*
