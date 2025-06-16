74554f530594c3ad3a118fb54a9b9b0b7ed4b44d
GianlucaPletti07 <gianlucapleyt@gmail.com>
2025-06-16 12:14:46 +0200

    Update FileTypeConverter.java

--- src/main/java/FileTypeConverter.java
+++ src/main/java/FileTypeConverter.java
@@ -1,5 +1,9 @@
 public class FileTypeConverter {
     public static void main(String[] args) {
         ConverterConfig config = new ConverterConfig();
+        Thread watcherThreader = new Thread(new DirectoryWatcher(directoryPath));
+        watcherThreader.setDaemon(true);
+        watcherThreader.start();
+        while(true){}
     }
 }
