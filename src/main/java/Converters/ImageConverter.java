93c289b1add5ed3f7fa83b49324e45984013ff4c
paoloferrari <paolo.ferrari.bs.2007@gmail.com>
2025-06-17 15:45:47 +0200

    ImageConverter yes 2

--- src/main/java/Converters/ImageConverter.java
+++ src/main/java/Converters/ImageConverter.java
@@ -13,52 +13,60 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.List;
 
-public class ImageConverter implements Converter{
+public class ImageConverter implements Converter {
 
     @Override
     public ArrayList<File> convert(File imgFile) throws IOException, DocumentException {
+        System.out.println("ciao bello");
         ArrayList<File> files = new ArrayList<>();
         String imgName = imgFile.getName();
         Pattern pattern = Pattern.compile("\\[\\[(.*?)\\]\\]");
         Matcher matcher = pattern.matcher(imgName);
-        String extracted = matcher.group(1);
-        File file = imageConversion(imgFile, extracted);
-        files.add(file);
+
+        if (matcher.find()) {
+            String extracted = matcher.group(1);
+            File file = imageConversion(imgFile, extracted);
+            files.add(file);
+        } else {
+            System.out.println("No match found in file name: " + imgName);
+        }
+
         return files;
     }
 
     public static File imageConversion(File imgFile, String extracted) throws IOException {
         List<String> estensioniTrasparenza = new ArrayList<>(Arrays.asList("png", "TIFF", "WebP"));
         BufferedImage image = ImageIO.read(imgFile);
+
+        if (image == null) {
+            throw new IOException("Immagine non valida o non supportata: " + imgFile.getName());
+        }
+
         File outFile = new File("src/temp", getName(imgFile) + "." + extracted);
         int lastDotIndex = imgFile.getName().lastIndexOf('.');
         String extension = imgFile.getName().substring(lastDotIndex + 1);
-        if(!estensioniTrasparenza.contains(extension) && !estensioniTrasparenza.contains(extracted)){
+
+        if (!estensioniTrasparenza.contains(extension) && !estensioniTrasparenza.contains(extracted)) {
             image = alphaChannelRemover(image);
         }
+
         ImageIO.write(image, extracted, outFile);
         return outFile;
     }
 
-    private static String getName(File inputFile){
+    private static String getName(File inputFile) {
         String temp = inputFile.getName();
         return temp.split("\\.")[0];
     }
 
-    private static BufferedImage alphaChannelRemover(BufferedImage inImage){
-        int colorComponent = inImage.getType();
-        switch (colorComponent){
-            case BufferedImage.TYPE_INT_ARGB:
-                colorComponent = BufferedImage.TYPE_INT_RGB;
-            case BufferedImage.TYPE_4BYTE_ABGR:
-                colorComponent = BufferedImage.TYPE_INT_BGR;
-        }
-        // Creo una copia di inImage per l'output
-        BufferedImage copy = new BufferedImage(inImage.getWidth(), inImage.getHeight(), colorComponent);
+    private static BufferedImage alphaChannelRemover(BufferedImage inImage) {
+        int imageType = BufferedImage.TYPE_INT_RGB;
+        BufferedImage copy = new BufferedImage(inImage.getWidth(), inImage.getHeight(), imageType);
         Graphics2D g2d = copy.createGraphics();
-        g2d.drawImage(inImage, 0, 0, null);
+        g2d.setComposite(AlphaComposite.SrcOver);
+        g2d.drawImage(inImage, 0, 0, Color.WHITE, null);
         g2d.dispose();
         return copy;
     }
-
 }
+
