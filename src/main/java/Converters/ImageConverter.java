684a6733f292eddf11104a6d91b763dfce85bfc7
Anton <cicalese.anton@gmail.com>
2025-06-17 14:17:23 +0200

    ImageConverter

---
+++ src/main/java/Converters/ImageConverter.java
@@ -0,0 +1,14 @@
+package Converters;
+
+import com.itextpdf.text.DocumentException;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class ImageConverter implements Converter{
+    @Override
+    public ArrayList<File> convert(File srcFile) throws IOException, DocumentException {
+        return null;
+    }
+}
