8bf4c669553676fcf8819d7f67992b12aa72a29d
GianlucaPletti07 <gianlucapleyt@gmail.com>
2025-06-17 10:18:38 +0200

    Update FileTypeConverter.java

---
+++ src/main/java/Converters/ZIPtoTARGZconverter.java
@@ -0,0 +1,12 @@
+package Converters;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class ZIPtoTARGZconverter implements Converter{
+    @Override
+    public ArrayList<File> convert(File srcFile) throws IOException {
+        return null;
+    }
+}
