8e82af7641bdda20df326df2a7ff6eb0bcba6173
paoloferrari <paolo.ferrari.bs.2007@gmail.com>
2025-06-18 17:36:33 +0200

    log vari

--- src/main/java/converter/Utility.java
+++ src/main/java/converter/Utility.java
@@ -1,6 +1,9 @@
 package converter;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -21,5 +24,16 @@ public class Utility {
         String nomeFile = file.getName();
         String estensioneInterna = estraiEstensioneInterna(file);
         return nomeFile.replace("-$$" + estensioneInterna + "$$-", "");
+    }
+
+    /**
+     * Copia il contenuto da un InputStream a un OutputStream
+     */
+    public static void copy(InputStream in, OutputStream out) throws IOException {
+        byte[] buffer = new byte[4096];
+        int bytesRead;
+        while ((bytesRead = in.read(buffer)) != -1) {
+            out.write(buffer, 0, bytesRead);
+        }
     }
 }
