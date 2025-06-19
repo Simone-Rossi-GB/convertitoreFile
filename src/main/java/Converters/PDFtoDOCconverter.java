c81d3970ac1b001d0b8596e9392bbace1752650a
paoloferrari <paolo.ferrari.bs.2007@gmail.com>
2025-06-19 15:14:40 +0200

    img fix

--- src/main/java/Converters/PDFtoDOCconverter.java
+++ src/main/java/Converters/PDFtoDOCconverter.java
@@ -2,6 +2,7 @@ package Converters;
 
 import java.util.ArrayList;
 
+import converter.Log;
 import org.apache.pdfbox.cos.COSDocument;
 import org.apache.pdfbox.io.RandomAccessBufferedFileInputStream;
 import org.apache.pdfbox.pdfparser.PDFParser;
@@ -22,36 +23,55 @@ public class PDFtoDOCconverter extends AbstractPDFConverter {
      */
     @Override
     protected ArrayList<File> convertInternal(File pdfFile, PDDocument pdfDocument) throws Exception {
+        // Validazione degli input
         validateInputs(pdfFile, pdfDocument);
-        try {
-            ArrayList<File> files = new ArrayList<>();
-            String baseName = pdfFile.getName().replaceAll("(?i)\\.pdf$", "");
-            File outputFile = new File(baseName + ".doc");
-            File template = new File("themplate.doc");
 
+        ArrayList<File> files = new ArrayList<>();
+        String baseName = pdfFile.getName().replaceAll("(?i)\\.pdf$", "");
+        File outputFile = new File(baseName + ".doc");
+        File template = new File("themplate.doc");
+
+        try {
+            // Controlla l'esistenza del template
             if (!template.exists()) {
-                throw new FileNotFoundException("Il file template 'themplate.doc' non esiste");
+                Log.addMessage("ERRORE: file template 'themplate.doc' non trovato");
+                throw new FileNotFoundException("Template mancante");
             }
+
+            Log.addMessage("Estrazione testo dal file PDF: " + pdfFile.getName());
             PDFTextStripper stripper = new PDFTextStripper();
             String text = stripper.getText(pdfDocument);
 
+            // Apertura del template Word e scrittura del testo estratto
             try (FileInputStream fis = new FileInputStream(template);
                  HWPFDocument doc = new HWPFDocument(fis);
                  FileOutputStream out = new FileOutputStream(outputFile)) {
 
+                Log.addMessage("Scrittura testo nel file DOC: " + outputFile.getName());
                 Range range = doc.getRange();
                 range.replaceText(range.text(), text);
                 doc.write(out);
 
                 files.add(outputFile);
+                Log.addMessage("Creazione file .doc completata: " + outputFile.getName());
                 return files;
 
             }
+        } catch (FileNotFoundException e) {
+            // Template o PDF mancante
+            Log.addMessage("ERRORE: file non trovato - " + e.getMessage());
+            throw e;
+
+        } catch (IOException e) {
+            Log.addMessage("ERRORE: problema I/O durante la conversione - " + e.getMessage());
+            throw new IOException("Errore I/O durante la conversione del file " + pdfFile.getName(), e);
 
         } catch (Exception e) {
-            throw new Exception("Errore durante il processo di conversione: " + e.getMessage());
+            Log.addMessage("ERRORE: eccezione durante la conversione - " + e.getMessage());
+            throw new Exception("Errore generico durante la conversione del file " + pdfFile.getName(), e);
         }
     }
+
 
 
 }
