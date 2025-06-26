a25ec6fc07c382dc1e1b8435efe0c25e83482ade
Kirito-C <149937627+Kirito-C@users.noreply.github.com>
2025-06-26 16:02:08 +0200

    protectedOutputPartially

--- src/main/java/gui/MainViewController.java
+++ src/main/java/gui/MainViewController.java
@@ -495,7 +495,7 @@ public class MainViewController {
 
             // DIMENSIONI AGGIORNATE
             configStage.setWidth(750);
-            configStage.setHeight(630);
+            configStage.setHeight(730);
 
             // Crea la scene
             Scene scene = new Scene(configWindow);
